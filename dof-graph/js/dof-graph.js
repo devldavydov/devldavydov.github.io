73b41baf88733b99aa695de57a51596e3494b4f5
devldavydov <devldavydov@yandex.ru>
2021-05-28 19:34:42 +0300

    Add dof-graph

--- dof-graph/js/dof-graph.js
+++ dof-graph/js/dof-graph.js
@@ -1,22 +1,92 @@
 $(document).ready(function() {
     function setFocalLength() {
-        var focalLengthIndex = $('#focalLengthIndex').val();
-        var focalLength = FOCAL_LENGTH_DATA[focalLengthIndex];
-        $('#focalLength').text(focalLength);
+        $('#focalLength').text(getFocalLength());
     }
 
     function setAperture() {
-        var apertureIndex = $('#apertureIndex').val();
-        var aperture = APERTURE_DATA[apertureIndex];
-        $('#aperture').text(aperture);
+        $('#aperture').text(getAperture());
     }
 
-    $(document).on('input', '#focalLengthIndex', setFocalLength);
-    $(document).on('input', '#apertureIndex', setAperture);
+    function setCoc() {
+        $('#coc').text(getCoc());
+    }
 
-    $('#focalLengthIndex').attr({max: FOCAL_LENGTH_DATA.length});
-    $('#apertureIndex').attr({max: APERTURE_DATA.length});
+    function getFocalLength() {
+        return FOCAL_LENGTH_DATA[$('#focalLengthIndex').val()];
+    }
+
+    function getAperture() {
+        return APERTURE_DATA[$('#apertureIndex').val()];
+    }
+
+    function getCoc() {
+        return COC_DATA[$('#cocIndex').val()];
+    }
+
+    function updateGraph(graph) {
+        var focalLength = getFocalLength();
+        var aperture = getAperture();
+        var coc = getCoc();
+
+        var dofNear = [];
+        var dofFar = [];
+        $.each(FOCUS_DISTANCE_DATA, function (index, focusDistance) {
+            var dofRange = dofCalculate(focusDistance, focalLength, aperture, coc);
+            dofNear.push(dofRange[0]);
+            dofFar.push(dofRange[1]);
+        });
+
+        graph.data.datasets[0].data = dofNear;
+        graph.data.datasets[1].data = dofFar;
+        graph.update()
+    }
+
+    $('#focalLengthIndex').attr({max: FOCAL_LENGTH_DATA.length - 1});
+    $('#apertureIndex').attr({max: APERTURE_DATA.length - 1});
+    $('#cocIndex').attr({max: COC_DATA.length - 1});
+
+    var dofGraph = new Chart(
+        document.getElementById('dofGraph'),
+        {
+          type: 'line',
+          data: {
+            labels: FOCUS_DISTANCE_DATA,
+            datasets: [
+                {
+                  label: 'DoF Near',
+                  fill: false,
+                  borderColor: '#0b5ed7',
+                  data: [],
+                  cubicInterpolationMode: 'monotone'
+                },
+                {
+                  label: 'DoF Far',
+                  fill: false,
+                  borderColor: '#dc3545',
+                  data: [],
+                  cubicInterpolationMode: 'monotone'
+                }
+            ]
+          },
+          options: {}
+        }
+    );
+
+    $(document).on('input', '#focalLengthIndex', function() {
+        setFocalLength();
+        updateGraph(dofGraph);
+    });
+    $(document).on('input', '#apertureIndex', function() {
+        setAperture();
+        updateGraph(dofGraph);
+    });
+    $(document).on('input', '#cocIndex', function() {
+        setCoc();
+        updateGraph(dofGraph);
+    });
 
     setFocalLength();
     setAperture();
+    setCoc();
+    updateGraph(dofGraph);
 });
--- js/common.js
+++ js/common.js
@@ -1,5 +1,7 @@
 var FOCAL_LENGTH_DATA = [17, 20, 28, 35, 55];
 var APERTURE_DATA = [2.8, 3.2, 3.5, 4, 4.5, 5, 5.6, 6.3, 7.1, 8, 9, 10, 11, 13, 14, 16, 18, 20, 22];
+var COC_DATA = [0.019, 0.03]
+var FOCUS_DISTANCE_DATA = [0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30];
 
 function dofCalculate(focusDistance, focalLength, aperture, circleOfConfusion) {
     // https://ru.wikipedia.org/wiki/%D0%93%D0%BB%D1%83%D0%B1%D0%B8%D0%BD%D0%B0_%D1%80%D0%B5%D0%B7%D0%BA%D0%BE_%D0%B8%D0%B7%D0%BE%D0%B1%D1%80%D0%B0%D0%B6%D0%B0%D0%B5%D0%BC%D0%BE%D0%B3%D0%BE_%D0%BF%D1%80%D0%BE%D1%81%D1%82%D1%80%D0%B0%D0%BD%D1%81%D1%82%D0%B2%D0%B0
